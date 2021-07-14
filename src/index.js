e9c3db6b953b605568fd94b88bf8c3a7f50312e5
TeresaJanaszek <teresa.janaszek@gmail.com>
2021-07-14 13:26:59 +0200

    Created ejs layout templates

--- src/index.js
+++ src/index.js
@@ -1,22 +1,36 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts')
 const crypto = require('crypto');
+const path = require('path')
 const { users, schedules } = require('./data');
-
 const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.use(expressLayouts)
+app.set('view engine', 'ejs');
+app.use(express.static('public/css'));
 app.use(express.json());
 
+app.use ((req, res, next) => {
+    res.locals.url = req.originalUrl;
+    res.locals.host = req.get('host');
+    res.locals.protocol = req.protocol;
+    next();
+});
+
 app.get('/', (req, res) => {
-  res.send('Welcome to our schedule website');
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
 });
 
 app.get('/users', (req, res) => {
-  res.send(users);
+  // res.send(users);
+  res.render('users', {title: 'Users', users: users})
 });
 
 app.get('/users/:userId', (req, res) => {
   const { userId } = req.params;
   const user = users[userId];
-  res.send(user);
+  res.render('user', {title: 'User', user})
 });
 
 app.get('/users/:userId/schedules', (req, res) => {
@@ -27,11 +41,11 @@ app.get('/users/:userId/schedules', (req, res) => {
       userSchedules.push(i);
     }
   }
-  res.send(userSchedules);
+  res.render('userSchedules', {title: 'User Schedules', userSchedules: userSchedules})
 });
 
 app.get('/schedules', (req, res) => {
-  res.send(schedules);
+  res.render('schedules', {title: 'Schedules', schedules: schedules})
 });
 
 app.post('/schedules', (req, res) => {
@@ -40,6 +54,7 @@ app.post('/schedules', (req, res) => {
   res.send(u);
 });
 
+app.use(require('body-parser').urlencoded({ extended: false })); //sprawdź czy to działa z curl -> działa
 app.post('/users', (req, res) => {
   const u = req.body;
   u.password = crypto.createHash('sha256').update(u.password).digest('hex');
