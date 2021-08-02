e84b0991cf57d2b3632d424f2b92158e168b176a
TeresaJanaszek <teresa.janaszek@gmail.com>
2021-08-02 19:40:00 +0200

    Created new structure of page, main page and details page for users and schedules

--- src/data.js
+++ src/data.js
@@ -58,3 +58,24 @@ module.exports.getHashedPassword = (password) => {
 module.exports.generateAuthToken = () => {
     return crypto.randomBytes(30).toString('hex');
 }
+
+module.exports.numberToWeekDay=(number) =>{
+    switch(number){
+        case 1:
+            return "Monday";
+        case 2:
+            return "Tuesday";
+        case 3:
+            return "Wednesday";
+        case 4:
+            return "Thursday";
+        case 5:
+            return "Friday";
+        case 6:
+            return "Saturday";
+        case 7:
+            return "Sunday";
+        default:
+            return "incorrect day"     
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,17 +2,23 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts')
 const crypto = require('crypto');
 const path = require('path')
-const { users, schedules, getHashedPassword, generateAuthToken } = require('./data');
-let bodyParser = require('body-parser')
+const { users, schedules, getHashedPassword, generateAuthToken, numberToWeekDay } = require('./data');
+const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser');
-let urlencodedParser = bodyParser.urlencoded({ extended: false })
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const dotenv = require("dotenv");
+dotenv.config();
+const DB_NAME = process.env.DB_NAME
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
 
 const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.use(expressLayouts)
 app.set('view engine', 'ejs');
 app.use(express.static('public/css'));
-app.use(express.json());
+// app.use(express.json());
+app.use(cookieParser())
 
 
 app.use(require('body-parser').urlencoded({ extended: true })); //sprawdź czy to działa z curl -> działa
@@ -24,12 +30,22 @@ app.use ((req, res, next) => {
     next();
 });
 
+app.use((req, res, next) => {
+  // Get auth token from the cookies
+  const authToken = req.cookies['AuthToken'];
+
+  // Inject the user to the request
+  req.user = authTokens[authToken];
+
+  next();
+});
+
 //Authtokens
 const authTokens = {};
 
 //Sequelize
 const Sequelize = require('sequelize')
-const sequelize = new Sequelize('mrcoffee', 'postgres', 'Galimatias3!', {
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: 'localhost',
   dialect: 'postgres',
 });
@@ -90,8 +106,7 @@ const User = sequelize.define('user', {
     });
     // Note: using `force: true` will drop the table if it already exists
     Schedule.belongsTo(User, {foreignKey: "user_id"});
-    //Schedule.belongsTo(User)
-    Schedule.sync({ force: true})
+    Schedule.sync({ force: false})
     
 
 app.get('/', (req, res) => {
@@ -99,7 +114,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/login', (req, res) => {
-  res.render('login', { title: 'Login'})
+  res.render('login', { title: 'Login', message: ''})
 });
 
 app.post('/login', async (req, res) => {
@@ -112,12 +127,12 @@ app.post('/login', async (req, res) => {
       })
       if(user[0] ){
         const authToken = generateAuthToken();
-        authTokens[authToken] = user;
+        authTokens[authToken] = user[0].dataValues;
         res.cookie('AuthToken', authToken);
-        res.json(authToken)
+        res.redirect('userPage')
       }
       else{
-        res.json("Nie ma go tuu")
+        res.render('login', {title: 'Login', message: 'Invalid email or password.', alertClass: "alert-danger"})
       }
   }catch(error){
     console.error(error)
@@ -125,15 +140,53 @@ app.post('/login', async (req, res) => {
 });
 
 app.get('/signup', (req, res) => {
-  res.render('signUp', { title: 'Sign up'})
+  res.render('signUp', { title: 'Sign up', message: ''})
 });
 
-app.post('/signup', (req, res) => {
-  const { email, firstname, lastname, password, confirmPassword } = req.body;
-  if(password===confirmPassword){
-    console.log("Tu jest niezrobione")
+app.post('/signup', async (req, res) => {
+  const u = req.body;
+  if(u.password===u.confirmPassword){
+    try{
+      const user = await User.findAll({
+        where: { email: u.email}
+        })
+        if(user[0]){
+          res.render('signUp', {title: 'Sign up', message: 'User is already registered.', alertClass: 'alert-danger'})
+        }
+        else{
+          try {
+            u.password = getHashedPassword(u.password)
+            const newUser = new User(u)
+            await newUser.save()
+            res.render('login', {title: 'login', message: 'Registration complete. Please login to continue.',alertClass: 'alert-success'})
+            } catch(error) {
+            console.error(error)
+            }
+        }
+    }catch(error){
+      console.error(error)
+    }
   }
-  res.render('signUp', { title: 'Sign up'})
+  else{
+    res.render('signUp', { title: 'Sign up', message:"Password does not match."})
+  }
+});
+
+app.get('/userPage', async (req, res) => {
+  if(!req.user){
+    res.render('login', { title: 'Login', message: "You have to login to see this page."})
+  }
+  else {
+    try {
+      const schedules = await Schedule.findAll()
+      for (const i of schedules) {
+        i.day = numberToWeekDay(i.day)
+      }
+      res.render('schedules', {title: 'User Page', schedules: schedules})
+      } catch(error) {
+      console.error(error)
+    }
+  }
 });
 
 app.get('/users', async (req, res) => {
@@ -145,48 +198,85 @@ app.get('/users', async (req, res) => {
   }
 });
 
-app.get('/users/:userId/schedules', async (req, res) => {
-  const { userId } = req.params;
-  try{
-    const s = await Schedule.findAll({
-      where: { id: userId }
-      })
-      if(s.length===0){
-        res.render('errorPage', {title: 'User Schedules', message: 'This user has no schedules'})
-      }
-      else{
+app.get('/userSchedules', async (req, res) => {
+  if(!req.user){
+    res.render('login', { title: 'Login', message: "You have to login to see this page."})
+  }
+  else {
+    const userId  = req.user.id
+
+    try{
+      const s = await Schedule.findAll({
+        where: { user_id: userId }
+        })
+        for (const i of s) {
+          i.day = numberToWeekDay(i.day)
+        }
         res.render('userSchedules', {title: 'User Schedules', userSchedules: s})
-      }
-  }catch(error){
-    console.error(error)
+    }catch(error){
+      console.error(error)
+    }
   }
 });
 
+
 app.get('/schedules', async (req, res) => {
-  try {
-  const schedules = await Schedule.findAll()
-  res.render('schedules', {title: 'Schedules', schedules: schedules})
-  } catch(error) {
-  console.error(error)
+  if(!req.user){
+    res.render('login', { title: 'Login', message: "You have to login to see this page."})
+  }
+  else {
+    try {
+    const schedules = await Schedule.findAll()
+    res.render('schedules', {title: 'Schedules', schedules: schedules})
+    } catch(error) {
+    console.error(error)
+    }
   }
 });
 
 app.get('/schedules/new', (req, res) => {
-  res.render('newSchedule', {title: 'Add new schedule'})
+  if(!req.user){
+    res.render('login', { title: 'Login', message: "You have to login to see this page."})
+  }
+  else {
+    res.render('newSchedule', {title: 'Add new schedule'})
+  }
+});
+
+app.get('/schedules/delete/:id', (req, res) => {
+  if(!req.user){
+    res.render('login', { title: 'Login', message: "You have to login to see this page."})
+  }
+  else {
+    const id_schedule = req.params.id;
+    console.log("Helloe: "+ id_schedule)
+    try{
+      Schedule.destroy({
+        where: {id: id_schedule}
+      })
+    }catch(error){
+      console.log(error)
+    }
+    res.redirect('/userSchedules')
+  }
 });
 
 app.post('/schedules/new', async (req, res) => {
-  try {
-    const s = req.body
-    const newSchedule = new Schedule(s)
-    await newSchedule.save()
-    res.redirect('/schedules') // Returns the new user that is created in the database
-    } catch(error) {
-    console.error(error)
-    }
-
-  //
-  const u = req.body;
+  if(!req.user){
+    res.render('login', { title: 'Login', message: "You have to login to see this page."})
+  }
+  else {
+    try {
+      const s = req.body
+      s.user_id=req.user.id
+      console.log(s)
+      const newSchedule = new Schedule(s)
+      await newSchedule.save()
+      res.redirect('/userSchedules') // Returns the new user that is created in the database
+      } catch(error) {
+      console.error(error)
+      }
+  }
 });
 
 app.get('/users/new', (req, res) => {
@@ -208,10 +298,13 @@ app.post('/users/new', urlencodedParser, async (req, res) => {
 app.get('/users/:userId', async (req, res) => {
   const {userId} = req.params
   try {
-  const u = await User.findAll({
+  const user = await User.findAll({
   where: { id: userId }
   })
-  res.render('user', {title: 'User', user: u[0]})
+  const userSchedules = await Schedule.findAll({
+    where: {user_id: userId}
+  })
+  res.render('user', {title: 'User', user: user[0], userSchedules: userSchedules})
   } catch(error) {
   console.error(error)
   }
